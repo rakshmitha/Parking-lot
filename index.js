3e2a7fb8e7926ee43cb98dfef093a52ca3132bc3
Rakshmitha M <rakshmitha17@gmail.com>
2020-11-22 14:39:08 +0530

    server connections established

--- index.js
+++ index.js
@@ -22,4 +22,4 @@ mongoose.connect(DBURI,
 
 app.use("/api", require("./routes"));
 
-//app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
